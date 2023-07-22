08740179a3afdcb5afd45f5c1cad797fda6da54e
lin <2352456947@qq.com>
2023-07-23 01:57:11 +0800

    add taro-ui-vue3

--- client/src/app.ts
+++ client/src/app.ts
@@ -1,9 +1,20 @@
 import { createApp } from 'vue'
 import './app.css'
+import 'taro-ui-vue3/dist/style/index.scss'
+
+import { createUI } from 'taro-ui-vue3'
+import { AtButton, AtInput, AtTabs } from 'taro-ui-vue3/lib'
 
 const App = createApp({
-  onShow (options) {},
+  onShow(options) {}
   // 入口组件不需要实现 render 方法，即使实现了也会被 taro 所覆盖
 })
+
+const tuv3 = createUI({
+  AtButton,
+  AtInput,
+  AtTabs
+})
+App.use(tuv3)
 
 export default App
